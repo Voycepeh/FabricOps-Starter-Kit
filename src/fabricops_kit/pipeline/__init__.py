fb0c48c868bc937e4bbd7023e70a58f19c569769
Voycevestern <127464175+Voycepeh@users.noreply.github.com>
2026-08-19 13:46:00 +0000

    Export Stage 4D guardrail orchestrator

--- src/fabricops_kit/pipeline/__init__.py
+++ src/fabricops_kit/pipeline/__init__.py
@@ -7,6 +7,7 @@ from fabricops_kit.pipeline.check_schema import check_schema
 from fabricops_kit.pipeline.check_freshness import check_freshness
 from fabricops_kit.pipeline.check_changes import check_changes
 from fabricops_kit.pipeline.check_dq import check_dq
+from fabricops_kit.pipeline.run_table_guardrails import run_table_guardrails
 from fabricops_kit.pipeline.observe_table import observe_table
 from fabricops_kit.pipeline.guardrails_shared import (
     enforce_freshness,
@@ -19,6 +20,7 @@ __all__ = [
     "check_freshness",
     "check_changes",
     "check_dq",
+    "run_table_guardrails",
     "observe_table",
     "enforce_freshness",
     "enforce_profile_behavior",
