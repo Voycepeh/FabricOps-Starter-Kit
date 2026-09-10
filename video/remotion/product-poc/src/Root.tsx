82c0ace0336350952c043dea5080547a60d9b4ec
Voycevestern <127464175+Voycepeh@users.noreply.github.com>
2026-09-10 13:31:58 +0800

    Tighten Remotion motion master timeline

--- video/remotion/product-poc/src/Root.tsx
+++ video/remotion/product-poc/src/Root.tsx
@@ -1,11 +1,12 @@
 import {Composition} from 'remotion';
 import {FabricOpsHero} from './FabricOpsHero';
+import {LONG_FORM_STORY_FRAMES} from './scenes/LongFormStory';
 
 export const Root = () => (
   <Composition
     id="FabricOpsHero"
     component={FabricOpsHero}
-    durationInFrames={12150}
+    durationInFrames={LONG_FORM_STORY_FRAMES}
     fps={30}
     width={1920}
     height={1080}
