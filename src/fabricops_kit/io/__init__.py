8257eca3ef86f905fe8c4093ffda34f1dc2ca582
Voycevestern <127464175+Voycepeh@users.noreply.github.com>
2026-09-20 23:40:30 +0800

    Export SQL endpoint query reader

--- src/fabricops_kit/io/__init__.py
+++ src/fabricops_kit/io/__init__.py
@@ -5,6 +5,7 @@ from .read_lakehouse_excel import read_lakehouse_excel
 from .read_lakehouse_json import read_lakehouse_json
 from .read_lakehouse_parquet import read_lakehouse_parquet
 from .read_lakehouse_table import read_lakehouse_table
+from .read_sql_endpoint_query import read_sql_endpoint_query
 from .read_warehouse_query import read_warehouse_query
 from .read_warehouse_table import read_warehouse_table
 from .write_lakehouse_table import write_lakehouse_table
@@ -16,6 +17,7 @@ __all__ = [
     "read_lakehouse_json",
     "read_lakehouse_parquet",
     "read_lakehouse_table",
+    "read_sql_endpoint_query",
     "read_warehouse_query",
     "read_warehouse_table",
     "write_lakehouse_table",
