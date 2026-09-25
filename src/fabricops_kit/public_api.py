308f170bc623df29200fc53a774e9958f90f96e7
Voycevestern <127464175+Voycepeh@users.noreply.github.com>
2026-09-26 04:37:40 +0800

    Wire activation widget into governance lifecycle

--- src/fabricops_kit/public_api.py
+++ src/fabricops_kit/public_api.py
@@ -25,7 +25,7 @@ SUPPORTED_PUBLIC_API = (
 # Preview public functions remain importable for evaluation, but are not part of
 # the supported compatibility surface until promoted in a future release.
 PREVIEW_PUBLIC_API = (
-    "fabricops_kit.widgets.widget_data_contract.widget_data_contract",
+    "fabricops_kit.widgets.widget_data_contract.widget_data_contract",\n    "fabricops_kit.widgets.widget_activate_data_contract.widget_activate_data_contract",
     "fabricops_kit.access_scanner.scan_workspace_access.scan_workspace_access",
     "fabricops_kit.access_scanner.scan_onelake_access.scan_onelake_access",
     "fabricops_kit.access_scanner.scan_sql_access.scan_sql_access",
