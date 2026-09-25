e0ef8505e7a649410c38b8a4a2906c6ee1b6f5ac
Voycevestern <127464175+Voycepeh@users.noreply.github.com>
2026-09-26 04:37:47 +0800

    Wire activation widget into governance lifecycle

--- tests/unit/test_widget_ui_layout.py
+++ tests/unit/test_widget_ui_layout.py
@@ -37,7 +37,7 @@ def _install_fake_notebook_widgets(monkeypatch):
 
 
 LIVE_WIDGETS = {
-    "widget_data_contract",
+    "widget_data_contract",\n    "widget_activate_data_contract",
     "widget_render_data_steward",
     "widget_render_data_agreement",
     "widget_select_data_contract",
